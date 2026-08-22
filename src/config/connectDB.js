880b91df61c3470883327fde786d0510e7ce9234
John Youssef <johnyoussef1337@gmail.com>
2026-08-22 03:35:45 +0300

    Add environment variables for database connection and update connection string

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function connectDB() {
     try {
-        const con = await mongoose.connect(process.env.PRODUCTION_DATABASE)
+        const con = await mongoose.connect(process.env.DB_CONNECTION_STRING)
         console.log(`Database is connected successfully at ${con.connection.name}`);
     } catch (error) {
         console.log(error);
