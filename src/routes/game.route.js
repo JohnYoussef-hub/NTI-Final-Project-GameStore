e974ca4f08824de67c50db9b2377529d4dfd3a9a
John Youssef <johnishere.4307@gmail.com>
2026-08-27 00:43:46 +0300

    feat: add authentication and authorization to game routes

--- src/routes/game.route.js
+++ src/routes/game.route.js
@@ -1,15 +1,19 @@
 const { getAllGames, getDeletedGames, getStatus, getOneGame, addGame, updateGame, deleteGame, softDeleteGame } = require("../controllers/game.controller");
+const auth = require("../middlewares/auth")
+const restrictTo = require("../middlewares/restrictTo")
 
 const router = require("express").Router()
 
-router.route("/").get(getAllGames).post(addGame);
+router.route("/").get(getAllGames).post(auth, restrictTo("admin"), addGame);
 
 router.route("/deleted-games").get(getDeletedGames);
 
 router.route("/status").get(getStatus);
 
-router.route("/:id").get(getOneGame).patch(updateGame).delete(deleteGame)
+router.route("/:id").get(getOneGame)
+	.patch(auth, restrictTo("admin"), updateGame)
+	.delete(auth, restrictTo("admin"), deleteGame)
 
-router.route("/:id/soft-deleted").patch(softDeleteGame)
+router.route("/:id/soft-deleted").patch(auth, restrictTo("admin"), softDeleteGame)
 
 module.exports = router;
