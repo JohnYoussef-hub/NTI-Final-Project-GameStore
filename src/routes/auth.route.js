ac8f48f8b7ca085af5f225362f3ee4db481e7fb6
MohamedMustafa29 <mm2673855@gmail.com>
2026-08-30 03:13:06 +0300

    Add remaining project folders and backend

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,13 +1,13 @@
-const { signUp, confirmEmail, login, forgetPassword, resetPassword } = require("../controllers/auth.controller")
+const { signUp, confirmEmail, resendOTP, login, forgetPassword, resetPassword } = require("../controllers/auth.controller")
 
 const router = require("express").Router()
 
-
 router.route("/signup").post(signUp)
 router.route("/confirm-email").post(confirmEmail)
+router.route("/resend-otp").post(resendOTP)
 router.route("/login").post(login)
 router.route("/forget-password").post(forgetPassword)
+router.route("/reset-password").post(resetPassword)
 router.route("/reset-password/:token").post(resetPassword)
-
 
 module.exports = router
